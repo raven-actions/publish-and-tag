53a7e3bf5729f5b59f98ac9ae1a20632e1e581c3
Dariusz Porowski <3431813+DariuszPorowski@users.noreply.github.com>
2025-06-19 22:28:35 -0700

    chore: lint (#284)

--- packages/actions-toolkit/tests/create-action.test.js
+++ packages/actions-toolkit/tests/create-action.test.js
@@ -10,8 +10,8 @@ jest.mock('enquirer')
 // Remove double quotes around snapshots.
 // This improves snapshot readability for generated file contents.
 expect.addSnapshotSerializer({
-  test: value => typeof value === 'string',
-  print: value => value
+  test: (value) => typeof value === 'string',
+  print: (value) => value
 })
 
 /** Creates a mock Signale logger instance for use in asserting log function calls. */
@@ -51,33 +51,26 @@ test('prints help when no arguments are passed', async () => {
   await runCLI()
 
   expect(process.exit).toHaveBeenCalledWith(1)
-  expect(logger.log).toHaveBeenCalledWith(
-    expect.stringMatching(/Usage: npx actions-toolkit <name>/)
-  )
+  expect(logger.log).toHaveBeenCalledWith(expect.stringMatching(/Usage: npx actions-toolkit <name>/))
 })
 
 test('prints help when --help is passed', async () => {
   await runCLI('--help')
 
   expect(process.exit).toHaveBeenCalledWith(1)
-  expect(logger.log).toHaveBeenCalledWith(
-    expect.stringMatching(/Usage: npx actions-toolkit <name>/)
-  )
+  expect(logger.log).toHaveBeenCalledWith(expect.stringMatching(/Usage: npx actions-toolkit <name>/))
 })
 
 test('fails to start creating project in a directory that already exists', async () => {
   await expect(runCLI('tests/fixtures/workspaces/action-already-exists')).rejects.toThrowError()
-  expect(logger.fatal).toHaveBeenCalledWith(
-    expect.stringMatching(/Folder .*tests\/fixtures\/workspaces\/action-already-exists already exists!/)
-  )
+  expect(logger.fatal).toHaveBeenCalledWith(expect.stringMatching(/Folder .*tests\/fixtures\/workspaces\/action-already-exists already exists!/))
 })
 
 test('fails to start when mkdir fails for an unknown reason', async () => {
-  jest.spyOn(fs, 'mkdir')
-    .mockImplementationOnce(() => { throw new Error('KABOOM') })
-  await expect(runCLI('__tmp/my-project-name'))
-    .rejects
-    .toThrowError('KABOOM')
+  jest.spyOn(fs, 'mkdir').mockImplementationOnce(() => {
+    throw new Error('KABOOM')
+  })
+  await expect(runCLI('__tmp/my-project-name')).rejects.toThrowError('KABOOM')
 })
 
 test('exits with a failure message when a user cancels the questionnaire', async () => {
@@ -105,29 +98,16 @@ test('creates project with labels passed to Dockerfile from questionnaire', asyn
     color: 'blue'
   })
 
-  const readGeneratedFile = name =>
-    fs.readFileSync(path.resolve(__dirname, '../__tmp/my-project-name', name), 'utf-8')
+  const readGeneratedFile = (name) => fs.readFileSync(path.resolve(__dirname, '../__tmp/my-project-name', name), 'utf-8')
 
   await runCLI('__tmp/my-project-name')
 
-  expect(logger.info).toHaveBeenCalledWith(
-    expect.stringMatching(/Creating folder .*my-project-name.../)
-  )
-  expect(logger.star).toHaveBeenCalledWith(
-    expect.stringMatching(/Welcome to actions-toolkit/)
-  )
-  expect(logger.info).toHaveBeenCalledWith(
-    expect.stringMatching(/Creating package.json/)
-  )
-  expect(logger.info).toHaveBeenCalledWith(
-    expect.stringMatching(/Creating Dockerfile/)
-  )
-  expect(logger.info).toHaveBeenCalledWith(
-    expect.stringMatching(/Creating index.js/)
-  )
-  expect(logger.info).toHaveBeenCalledWith(
-    expect.stringMatching(/Creating index.test.js/)
-  )
+  expect(logger.info).toHaveBeenCalledWith(expect.stringMatching(/Creating folder .*my-project-name.../))
+  expect(logger.star).toHaveBeenCalledWith(expect.stringMatching(/Welcome to actions-toolkit/))
+  expect(logger.info).toHaveBeenCalledWith(expect.stringMatching(/Creating package.json/))
+  expect(logger.info).toHaveBeenCalledWith(expect.stringMatching(/Creating Dockerfile/))
+  expect(logger.info).toHaveBeenCalledWith(expect.stringMatching(/Creating index.js/))
+  expect(logger.info).toHaveBeenCalledWith(expect.stringMatching(/Creating index.test.js/))
   expect(readGeneratedFile('package.json')).toMatchSnapshot('package.json')
   expect(readGeneratedFile('Dockerfile')).toMatchSnapshot('Dockerfile')
   expect(readGeneratedFile('index.js')).toMatchSnapshot('index.js')
